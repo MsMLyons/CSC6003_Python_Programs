ac95f9608a5d7c9d1325b26d72e78d5ecbaef08b
MsMLyons <ms.markilyons@gmail.com>
2023-10-31 21:30:48 -0400

    Update Module 1

--- Module_1/Practice/calculations_with_radius.py
+++ Module_1/Practice/calculations_with_radius.py
@@ -21,36 +21,41 @@ radius = float(input("Please enter a radius: "))
 print()
 sleep(1.0)
 
-# confirm user input of radius
-print(f"You entered {radius} as the radius, so let's get started. \n")
-sleep(1.0)
+# check radius is greater than zero
+if radius > 0: 
 
-# calculate circumference and print results
-def calc_circumference():
-    circumference = 2*math.pi*radius
-    round_circumference = round(circumference, 5)
-    print(f"The circumference of a circle with a radius of {radius} is {round_circumference} \n")
+    # confirm user input of radius
+    print(f"You entered {radius} as the radius, so let's get started. \n")
+    sleep(1.0)
 
-calc_circumference()
-sleep(1.5)
+    # calculate circumference and print results
+    def calc_circumference():
+        circumference = 2*math.pi*radius
+        round_circumference = round(circumference, 5)
+        print(f"The circumference of a circle with a radius of {radius} is {round_circumference} \n")
 
-# calculate area and print results
-def calc_area():
-    area = math.pi*radius**2
-    round_area = round(area, 5)
-    print(f"The area of a circle with a radius of {radius} is {round_area} \n")
-    
-calc_area()
-sleep(1.5)
+    # calculate area and print results
+    def calc_area():
+        area = math.pi*radius**2
+        round_area = round(area, 5)
+        print(f"The area of a circle with a radius of {radius} is {round_area} \n")
 
-# calculate volume and print results
-def calc_volume():
-    volume = 4/3*math.pi*radius**3
-    round_volume = round(volume, 5)
-    print(f"The volume of a sphere with a radius of {radius} is {round_volume} \n")
+    # calculate volume and print results
+    def calc_volume():
+        volume = 4/3*math.pi*radius**3
+        round_volume = round(volume, 5)
+        print(f"The volume of a sphere with a radius of {radius} is {round_volume} \n")
 
-calc_volume()
-sleep(1.2)
+    calc_circumference()
+    sleep(1.5)
 
-# print outro salutation to user
-print("Great job! I hope you enjoyed making these calculations!")
+    calc_area()
+    sleep(1.5)
+
+    calc_volume()
+    sleep(1.2)
+
+    # print outro salutation to user
+    print("Great job! I hope you enjoyed making these calculations!")
+else:
+    print("Please try again later with a radius value greater than zero.")
