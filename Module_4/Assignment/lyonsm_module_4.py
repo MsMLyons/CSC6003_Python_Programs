6d89a4f8e72107c7757fef8af4938d41a7f45782
MsMLyons <ms.markilyons@gmail.com>
2023-11-21 19:43:01 -0500

    Update Module 4

--- Module_4/Assignment/lyonsm_module_4.py
+++ Module_4/Assignment/lyonsm_module_4.py
@@ -24,9 +24,8 @@ shape and prints the results for the user.
 """
 
 class Rectangle():
-    """ Rectangle class with length and width attributes.
-        This class calculates the perimeter and area of a
-        rectangle based on user input of length and width.
+    """ Rectangle class with length and width attributes. This class calculates the 
+        perimeter and area of a rectangle based on user input of length and width.
     """
     def __init__(self):
         """ Prompts the user for input and creates a rectangle """
@@ -53,11 +52,9 @@ class Rectangle():
         print("The area of the rectangle is:", self.area())        
 
 class Parallelepiped(Rectangle):
-    """ The Parallelepiped class inherits from the Rectangle class. 
-        It reuses previous user input and prompts for the height
-        of the parallelepiped to calculate the volume of the shape.
-    """
+    """ The Parallelepiped class inherits from the Rectangle class. """
     def __init__(self):
+        """ Reuses previous user input and prompts for the height of the parallelepiped """
         print("\nGreat! Now let's calculate the volume of a parallelepiped.")
         print("We'll base it on the same length and width of the rectangle.\n")
         # reassign inherited attributes to be used in this class
