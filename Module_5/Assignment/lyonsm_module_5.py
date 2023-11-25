389a411237a2199f7df72a62e9ee1e5f97db6ea9
MsMLyons <ms.markilyons@gmail.com>
2023-11-25 09:20:11 -0500

    Update Module 5

--- Module_5/Assignment/lyonsm_module_5.py
+++ Module_5/Assignment/lyonsm_module_5.py
@@ -23,31 +23,37 @@ instance of each object.
 """
 
 class Personnel:
-    """ Create the Personnel class with the attributes: name, age, and hourly_rate"""
+    """ Create the Personnel class with the attributes: name, age, and hourly_rate """
     def __init__(self, name, age, hourly_rate):
         self.name = name
         self.age = age
         self.hourly_rate = hourly_rate
 
     def display(self):
-        """ Create a method to display Personnel data once an object is instantiated """
+        """ Create a method to display Personnel attributes once an object is instantiated 
+        from a child class 
+        """
         return self.name, self.age, self.hourly_rate
 
 class Doctor(Personnel):
-    """ Create child class of Personnel for Doctor with inherited attributes and methods """
+    """ Create a child class of the Personnel class called Doctor. The Doctor class inherits 
+        attributes and methods from the Personnel class, and adds the new attribute: specialty
+    """
     def __init__(self, name, age, hourly_rate, specialty):
         Personnel.__init__(self, name, age, hourly_rate)
         self.specialty = specialty
 
     def display_doctor(self):
-        """ Create a method that inherits from Personnel.display() to display 
+        """ Create a method that inherits from the Personnel.display() method to display 
             the data of an instantiated Doctor object
         """
         Personnel.display(self)
         print(f"\nDr {self.name} is {self.age} years old, makes ${self.hourly_rate} an hour, and their specialty is {self.specialty}.") 
                 
 class Surgeon(Personnel):
-    """ Create child class of Personnel for Surgeon with inherited attributes and methods """
+    """ Create a child class of Personnel for Surgeon with inherited attributes and methods. This class 
+        also displays the board certification status of the Surgeon object
+    """
     def __init__(self, name, age, hourly_rate):
         Personnel.__init__(self, name, age, hourly_rate)        
 
@@ -66,25 +72,30 @@ class Surgeon(Personnel):
         print(f"\nDr {self.name} is {self.age} years old, and makes ${self.hourly_rate} an hour.")
 
 class Nurse(Personnel):
-    """ Create child class of Personnel for Nurse with inherited attributes and methods """
+    """ Create a child class of Personnel for Nurse with inherited attributes and methods. This class
+        also displays the ranking of the nurse instantiated through the Nurse object
+    """
     def __init__(self, name, age, hourly_rate, rank=0):
         Personnel.__init__(self, name, age, hourly_rate)
         self.rank = rank 
 
     def display_nurse(self):
-        """ Method to display the data of an instantiated nurse object
-            that inherits from Personnel.display()        
+        """ Method to display the data of an instantiated nurse object that inherits from 
+        Personnel.display() 
         """
         Personnel.display(self)
         print(f"\nNurse {self.name} is {self.age} years old, and makes ${self.hourly_rate} an hour.")
         print(f"Nurse {self.name} is ranked #{self.rank} amongst the nursing staff.")
 
+# instantiate a doctor object
 new_doctor = Doctor("Alicia", 27, 300, "rheumatology")
 new_doctor.display_doctor()
 
+# instatiate a surgeon object
 new_surgeon = Surgeon("Patricia", 32, 345)
 new_surgeon.display_surgeon()
 new_surgeon.board_certification(board_certified=False)
 
+# instantiate a nurse object
 new_nurse = Nurse("Thomas", 38, 150, rank=3)
 new_nurse.display_nurse()
