f2a1536f83a682bae3a41fae1a4e1083f4f77224
MsMLyons <ms.markilyons@gmail.com>
2023-12-03 08:32:25 -0500

    Update Module 6

--- Module_6/Assignment_Practice/basic_music.py
+++ Module_6/Assignment_Practice/basic_music.py
@@ -4,6 +4,20 @@ music_collection = {
     "Only a Snack" : "The Mighty Teens",
 }
 
+# access a value using a tuple key
+musical_artist = music_collection[("Why Not?")]
+print(f"\nThe song 'Why Not?' is sung by {musical_artist}.")
+
+# output --> The song 'Why Not?' is sung by The Little Kids.
+
+# iterate over the dictionary and print a list of the songs
 print("\nThese are the songs in my music collection:\n")
 for title, artist in music_collection.items():    
     print(f"{title} by {artist}")
+
+# output --> 
+    # These are the songs in my music collection:
+
+    # Don't Eat My Tortilla Chips by Jack & Anita
+    # Why Not? by The Little Kids
+    # Only a Snack by The Mighty Teens
